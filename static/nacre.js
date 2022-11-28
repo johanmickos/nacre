6033ef419b12acabfeec66e09d8a5133ebdc8cde
Johan Mickos <johan.mickos@gmail.com>
2022-11-28 14:37:44 -0500

    Add static frontend assets

---
+++ static/nacre.js
@@ -0,0 +1,21 @@
+(function() {
+    const terminal = new Terminal({
+        convertEol: true,
+        scrollback: 10_000,
+        disableStdin: true,
+        cursorBlink: true,
+    });
+    const fitAddon = new FitAddon.FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.open(document.getElementById('terminal'), {focus: true});
+    fitAddon.fit();
+    window.addEventListener('resize', function() {
+        fitAddon.fit();
+    });
+    const url = 'ws://' + window.location.host + '/websocket';
+    const socket = new WebSocket(url);
+    socket.binaryType = 'arraybuffer';
+    const decoder = new TextDecoder('utf-8');
+    socket.onmessage = function(ev) { terminal.write(decoder.decode(ev.data)); };
+    socket.onopen = function() { socket.send(feedId); };
+}());
