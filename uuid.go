d2dfaa57568ee360be90cf37113ee145b3a8318d
Johan Mickos <johan.mickos@gmail.com>
2022-11-29 16:32:47 -0500

    Implement NewUUID

--- uuid.go
+++ uuid.go
@@ -1,6 +1,22 @@
 package nacre
 
+import (
+	"crypto/rand"
+	"fmt"
+)
+
 // NewUUID returns a new universally unique identifier.
 func NewUUID() string {
-	return "TODO"
+	buf := make([]byte, 16)
+	_, err := rand.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf(
+		"%x-%x-%x-%x",
+		buf[0:4],
+		buf[4:8],
+		buf[8:12],
+		buf[12:16],
+	)
 }
