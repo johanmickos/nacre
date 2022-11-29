6d2e3c5b93c84879546408dff42eadec7c587ea1
Johan Mickos <johan.mickos@gmail.com>
2022-11-28 23:28:49 -0500

    Ad listen/getAll to dummy storage impl

--- storage.go
+++ storage.go
@@ -3,11 +3,14 @@ package nacre
 import (
 	"context"
 	"log"
+	"time"
 )
 
 // Storage abstraction for handling streamed data.
 type Storage interface {
 	Push(ctx context.Context, id string, data []byte) error
+	Listen(ctx context.Context, id string) (<-chan []byte, error)
+	GetAll(ctx context.Context, id string) ([][]byte, error)
 }
 
 type loggingStorage struct{}
@@ -22,4 +25,41 @@ func NewLoggingStorage() Storage {
 func (s loggingStorage) Push(ctx context.Context, id string, data []byte) error {
 	log.Printf("[%s] Pushed %d bytes of data", id, len(data))
 	return nil
+}
+
+// Listen returns a channel that is populated with dummy data by a separate goroutine.
+func (s loggingStorage) Listen(ctx context.Context, id string) (<-chan []byte, error) {
+	ch := make(chan []byte)
+	go func() {
+		defer close(ch)
+		defer func() { log.Printf("Ending listen loop for dummy data\n") }()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default: // OK
+			}
+			time.Sleep(time.Second * 1)
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- []byte("dummy data from nacre\n"): // OK
+			case <-time.After(time.Second * 10):
+				log.Printf("Timeout while writing data in loggingStorage")
+				return
+			default: // OK
+			}
+		}
+	}()
+	return ch, nil
+}
+
+// GetAll returns a slice of dummy data.
+func (s loggingStorage) GetAll(ctx context.Context, id string) ([][]byte, error) {
+	return [][]byte{
+		[]byte("dummy data from nacre\n"),
+		[]byte("dummy data from nacre\n"),
+		[]byte("dummy data from nacre\n"),
+		[]byte("dummy data from nacre\n"),
+	}, nil
 }
