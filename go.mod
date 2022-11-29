c8ce6be00102a9bc3028b9a5db88ce209c7b5b16
Johan Mickos <johan.mickos@gmail.com>
2022-11-29 16:33:57 -0500

    Add Redis-based storage impl

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require golang.org/x/sync v0.1.0
 
 require (
+	github.com/go-redis/redis/v9 v9.0.0-rc.2
 	github.com/gorilla/websocket v1.5.0
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
 	honnef.co/go/tools v0.3.3
@@ -12,8 +13,10 @@ require (
 
 require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	golang.org/x/exp/typeparams v0.0.0-20220218215828-6cf2b201936e // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/tools v0.1.11-0.20220513221640-090b14e8501f // indirect
 )
---
+++ storage_redis.go
@@ -0,0 +1,112 @@
+package nacre
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+)
+
+const (
+	maxRedisStreamLen = 100
+	blockTimeout      = time.Second * 5
+)
+
+type redisStorage struct {
+	client *redis.Client
+}
+
+var _ Storage = (*redisStorage)(nil)
+
+// NewRedisStorage allocates a new Redis-backed storage implementation.
+func NewRedisStorage(client *redis.Client) Storage {
+	return &redisStorage{
+		client: client,
+	}
+}
+
+func (s *redisStorage) Push(ctx context.Context, id string, data []byte) error {
+	args := &redis.XAddArgs{
+		Stream: streamName(id),
+		MaxLen: maxRedisStreamLen,
+		Approx: true,
+		// TODO Add relevant metadata to entries
+		Values: map[string]any{
+			"data": data,
+		},
+		ID: "*",
+	}
+	return s.client.XAdd(ctx, args).Err()
+}
+
+func (s *redisStorage) Listen(ctx context.Context, id string) (<-chan []byte, error) {
+	ch := make(chan []byte)
+
+	go func() {
+		defer close(ch)
+
+		stream := streamName(id)
+		args := &redis.XReadArgs{
+			Streams: []string{stream, "0"},
+			Block:   -1,
+		}
+		streamData, err := s.client.XRead(ctx, args).Result()
+		if err != nil {
+			return
+		}
+		messages := streamData[0].Messages
+		for _, msg := range messages {
+			data := []byte(msg.Values["data"].(string))
+			select {
+			case ch <- data: // OK
+			case <-ctx.Done():
+				return
+			}
+		}
+		lastSeenID := messages[len(messages)-1].ID
+		for {
+			args := &redis.XReadArgs{
+				Streams: []string{stream, lastSeenID},
+				Block:   blockTimeout,
+			}
+			streamData, err := s.client.XRead(ctx, args).Result()
+			if err != nil {
+				return
+			}
+			messages := streamData[0].Messages
+			lastSeenID = messages[len(messages)-1].ID
+			for _, msg := range messages {
+				data := []byte(msg.Values["data"].(string))
+				select {
+				case ch <- data: // OK
+				case <-ctx.Done():
+					return
+				}
+			}
+
+		}
+	}()
+
+	return ch, nil
+}
+
+func (s *redisStorage) GetAll(ctx context.Context, id string) ([][]byte, error) {
+	stream := streamName(id)
+	args := &redis.XReadArgs{
+		Streams: []string{stream, "0"},
+		Block:   -1,
+	}
+	streamData, err := s.client.XRead(ctx, args).Result()
+	if err != nil {
+		return nil, err
+	}
+	results := make([][]byte, len(streamData[0].Messages))
+	for i, msg := range streamData[0].Messages {
+		data := msg.Values["data"]
+		results[i] = []byte(data.(string))
+	}
+	return results, nil
+}
+
+func streamName(id string) string { return fmt.Sprintf("nacre:feed:%s", id) }
