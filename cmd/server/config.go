c2d9190da895475f00753dbb055dabfb7a87c62b
Johan Mickos <johan.mickos@gmail.com>
2022-12-08 15:28:53 -0500

    Update Config to get environment variables on init

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -39,7 +42,38 @@ func ParseConfig() (Config, error) {
 			MaxStreamPersistence: time.Hour * 24,
 		},
 	}
-	// TODO Load from environment variables
+	if v := os.Getenv("NACRE_TCP_ADDR"); v != "" {
+		cfg.App.TCPAddr = v
+	}
+	if v := os.Getenv("NACRE_HTTP_ADDR"); v != "" {
+		cfg.App.HTTPAddr = v
+	}
+	if v := os.Getenv("NACRE_BASE_URL"); v != "" {
+		cfg.App.BaseURL = v
+	}
+	if v := os.Getenv("NACRE_MAX_STREAM_LEN"); v != "" {
+		maxLen, err := strconv.Atoi(v)
+		if err != nil {
+			return cfg, fmt.Errorf("NACRE_MAX_STREAM_LEN invalid: %w", err)
+		}
+		cfg.App.MaxRedisStreamLen = maxLen
+	}
+	if v := os.Getenv("NACRE_MAX_STREAM_PERSISTENCE"); v != "" {
+		persistDur, err := time.ParseDuration(v)
+		if err != nil {
+			return cfg, fmt.Errorf("NACRE_MAX_STREAM_PERSISTENCE invalid: %w", err)
+		}
+		cfg.App.MaxStreamPersistence = persistDur
+	}
+	if v := os.Getenv("NACRE_REDIS_HOST"); v != "" {
+		cfg.Redis.Host = v
+	}
+	if v := os.Getenv("NACRE_REDIS_PORT"); v != "" {
+		cfg.Redis.Port = v
+	}
+	if v := os.Getenv("NACRE_REDIS_PASSWORD"); v != "" {
+		cfg.Redis.Password = v
+	}
 	return cfg, nil
 }
 
